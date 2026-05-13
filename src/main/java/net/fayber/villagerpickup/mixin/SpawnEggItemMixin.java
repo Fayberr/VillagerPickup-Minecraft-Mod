636d2505de6f05b1d8939ccee0575c4046d6f9d2
Fayber <deine@email.de>
2026-05-14 01:59:25 +0200

    Fix: Explicitly handle vanilla baby spawning for non-trapped eggs. Cleaned up redundant mixins. Bumped version to 4.0.8.

--- src/main/java/net/fayber/villagerpickup/mixin/SpawnEggItemMixin.java
+++
@@ -1,8 +0,0 @@
-package net.fayber.villagerpickup.mixin;
-
-import net.minecraft.item.Item;
-import org.spongepowered.asm.mixin.Mixin;
-
-@Mixin(Item.class)
-public class SpawnEggItemMixin {
-}
--- src/main/java/net/fayber/villagerpickup/mixin/VillagerMixin.java
+++
@@ -1,8 +0,0 @@
-﻿package net.fayber.villagerpickup.mixin;
-
-import net.minecraft.entity.passive.VillagerEntity;
-import org.spongepowered.asm.mixin.Mixin;
-
-@Mixin(value = VillagerEntity.class, priority = 10000)
-public abstract class VillagerMixin {
-}
